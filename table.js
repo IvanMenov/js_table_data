71ce0f23f203f64df6ab537c032608b3e9b79fe9
Ivan Menov <vanetomenov@gmail.com>
2017-12-20 21:45:58 +0200

    add reverse sorting and arrows

--- table.js
+++ table.js
@@ -4,6 +4,9 @@ class Table{
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+		if(document.querySelector('tr')){
+			return;
+		}
      var data=JSON.parse(this.responseText);
 	 let table=createTable(data.rows.length, data.columns.length);
 	 
@@ -34,19 +37,44 @@ class Table{
 			let index= getIndexOfTheCorrectData(data);
 			let value=checkIsAscendAttributeValue(event);
 			
-				
+			if(value){
 				if(isNaN(a.querySelectorAll('td')[index].innerHTML.toLowerCase())){
-					if(a.querySelectorAll('td')[index].innerHTML.toLowerCase() < b.querySelectorAll('td')[index].innerHTML.toLowerCase()) return -1;
-					if(a.querySelectorAll('td')[index].innerHTML.toLowerCase() > b.querySelectorAll('td')[index].innerHTML.toLowerCase()) return 1;
-					return 0;
+					if(a.querySelectorAll('td')[index].innerHTML.toLowerCase() < b.querySelectorAll('td')[index].innerHTML.toLowerCase()){
+						return -1;
+					} 
+					if(a.querySelectorAll('td')[index].innerHTML.toLowerCase() > b.querySelectorAll('td')[index].innerHTML.toLowerCase()){
+						return 1;
+					} 
+					else{
+						return 0;
+					}
+					
 				}
 				else{
 					return ((a.querySelectorAll('td')[index].innerHTML)-(b.querySelectorAll('td')[index].innerHTML));
 				}
+			}	
+			else{
+				if(isNaN(a.querySelectorAll('td')[index].innerHTML.toLowerCase())){
+					if(a.querySelectorAll('td')[index].innerHTML.toLowerCase() < b.querySelectorAll('td')[index].innerHTML.toLowerCase()){
+						return 1;
+					} 
+					if(a.querySelectorAll('td')[index].innerHTML.toLowerCase() > b.querySelectorAll('td')[index].innerHTML.toLowerCase()){
+						return -1;
+					} 
+					else{
+						return 0;
+					}
+					
+				}
+				else{
+					return ((b.querySelectorAll('td')[index].innerHTML)-(a.querySelectorAll('td')[index].innerHTML));
+				}
+			}	
 			
 			
 		function checkIsAscendAttributeValue(event){
-			return event.target.getAttribute('isAscend');
+			return event.target.getAttribute('isAscend')==='true';
 				
 			
 		}	
@@ -57,7 +85,7 @@ class Table{
 		function getIndexOfTheCorrectData(data){
 			let sth=a.querySelectorAll('td');
 			for(let index=0;index<sth.length;index++){
-				if(sth[index].getAttribute('class')===data){
+				if(data.startsWith(sth[index].getAttribute('class'))){
 					return index;
 				}
 			}
@@ -65,15 +93,45 @@ class Table{
 	}
 		
 	);
-	for(let i=0;elements.length;i++){
+	for(let i=0;i<elements.length;i++){
 		document.querySelector('tbody').appendChild(elements[i]);
 	}
 	elements=null;
+	removeArrow();
+	addArrow();
+	changeIsAscendAttributeValue();
+	
+	
+	
+	function addArrow(){
+		if(event.target.getAttribute('isascend')==='true'){
+			event.target.innerHTML=event.target.innerHTML +" &#x2193;";
+		}else{
+			event.target.innerHTML=event.target.innerHTML +" &#x2191;";
+		}
+	}
+	function removeArrow(){
+		let unicodeValue=event.target.innerHTML.charCodeAt(event.target.innerHTML.length-1);
+		if(unicodeValue===8595 || unicodeValue===8593){
+			event.target.innerHTML=event.target.innerHTML.substring(0, event.target.innerHTML.length-2);
+		}
+		
+	}
+	function changeIsAscendAttributeValue(){
+		let currentValue=event.target.getAttribute('isascend');
+		if(currentValue==='true'){
+			event.target.setAttribute("isascend", "false");
+		}else{
+			event.target.setAttribute("isascend", "true");
+		}
+			
+	}
   }
 	
 	function fillTable(table,data){
 		 for(let i=0;i<data.columns.length;i++){
-		 table.rows[0].cells[i].innerHTML=data.columns[i];
+		 table.rows[0].cells[i].innerHTML=data.columns[i] ;
+		 
 	 }
 	 for(let i=1;i<table.rows.length;i++){
 		for(let j=0;j<table.rows[0].cells.length;j++){
